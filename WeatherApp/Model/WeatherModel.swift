a2373c187457b9332ce3ea8bfb7c4e74d845c90e
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-05-18 22:24:05 +0300

    add models

--- WeatherApp/Model/WeatherModel.swift
+++ WeatherApp/Model/WeatherModel.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+struct Location: Codable {
+    let name: String?
+    let region: String?
+    let country: String?
+}
+
+struct Weather: Codable {
+    let current: Current?
+    let forecast: Forecast?
+}
+
+struct Current: Codable {
+    let condition: Condition?
+    let temp_c: Double?
+    let wind_kph: Double?
+    let humidity: Double?
+}
+
+struct Forecast: Codable {
+    let forecastday: [Forecastday]?
+}
+
+struct Forecastday: Codable {
+    let date: String?
+    let day: Day?
+}
+
+struct Day: Codable {
+    let avgtemp_c: Double?
+    let maxwind_kph: Double?
+    let avghumidity: Double?
+    let condition: Condition?
+}
+
+
+struct Condition: Codable {
+    let text: String?
+    let icon: String?
+}
