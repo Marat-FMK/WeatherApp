a35ec0f815da18d145188f9c1f5166b834651b76
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-05-19 20:13:37 +0300

    add Home View elements

--- WeatherApp/HomeView/TemperatureStack.swift
+++ WeatherApp/HomeView/TemperatureStack.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct TemperatureStack: View {
+    let temperature: Double
+    let selectedScale: String
+    let selectScale: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            HStack(alignment: .top) {
+                Text(String(temperature))
+                    .customFont(name: .pillGothic600mgThin, size: 120)
+                    .foregroundStyle(.white)
+                    .shadow(color: .gray.opacity(0.6), radius: 1, x: -5, y: 3)
+                
+                Text(selectedScale == "Celsius" ? "ºC" : "ºF")
+                    .customFont(name: .pillGothic600mgLight, size: 20)
+                    .foregroundStyle(.white.opacity(0.6))
+                    .shadow(color: .gray.opacity(0.6), radius: 1, x: -5, y: 3)
+            }
+            
+            Spacer()
+            
+            VStack(spacing: -8) {
+                ScaleSelectionButton(selectedScale: selectedScale, buttonScale: "Celsius", selectScale: selectScale)
+                
+                ScaleSelectionButton(selectedScale: selectedScale, buttonScale: "Fahrenheit", selectScale: selectScale)
+            }
+        }
     }
 }
 
 #Preview {
-    TemperatureStack()
+    TemperatureStack(temperature: 31.7, selectedScale: "Fahrenheit", selectScale: {})
 }
--- WeatherApp/HomeView/WeatherIcon.swift
+++ WeatherApp/HomeView/WeatherIcon.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct WeatherIcon: View {
+    @State private var presentDescription = false
+    let url: String
+    let description: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            AsyncImage(url: URL(string: "https:" + (url))) { Icon in
+                Icon
+                    .resizable()
+                    .scaledToFit()
+//                    .frame(height: 120)
+                    .shadow(color: .blue, radius: 15, x: -20, y: 8)
+                
+            } placeholder: {
+                Image(systemName: "tropicalstorm.circle")
+                    .resizable()
+                    .scaledToFit()
+//                    .frame(height: 120)
+                    .foregroundStyle(.white.opacity(0.5))
+            }
+            
+            Spacer()
+            
+            Button {
+                withAnimation {
+                    presentDescription.toggle()
+                }
+            } label: {
+                if presentDescription {
+                    Text(description)
+                        .customFont(name: .pillGothic600mgBold, size: 20)
+                        .minimumScaleFactor(0.5)
+                        .lineLimit(2)
+                        .foregroundStyle(.white.opacity(0.6))
+                } else {
+                Image(systemName: "questionmark.circle.dashed")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.white.opacity(0.2))
+                    .frame(width: 60)
+            }
+            }
+            .buttonStyle(.plain)
+        }
+        .frame(height: 120)
     }
 }
 
 #Preview {
-    WeatherIcon()
+    WeatherIcon(url: "//cdn.weatherapi.com/weather/64x64/day/176.png", description: "Sunny / rain")
 }
