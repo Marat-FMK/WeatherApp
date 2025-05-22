7f5e8e17560184ff68af2d5ce24dec6f2330ad3a
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-05-22 03:08:38 +0300

    fix model for work with hasable

---
+++ WeatherApp/HomeView/Subviews/CityName.swift
@@ -0,0 +1,18 @@
+//
+//  CityName.swift
+//  WeatherApp
+//
+//  Created by Marat Fakhrizhanov on 22.05.2025.
+//
+
+import SwiftUI
+
+struct CityName: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CityName()
+}
